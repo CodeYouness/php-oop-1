a9d0087f0993542a80e94ed67ac4a493d7d3185a
CodeYouness <ylijassi@gmail.com>
2024-07-03 18:27:48 +0200

    added on page the 2 new movie

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ require_once __DIR__ . '/classes/Movie.php';
 
 $filmfiko = new Movie("l'arrivo dello scemo", "Biografia", "tanto");
 $filmnoioso = new Movie("Piove cioccolata", "Comico", "tanto corto");
+
+var_dump($filmfiko, $filmnoioso)
 ?>
