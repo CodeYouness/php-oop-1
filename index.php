55523a835689b35919deb5b2877710101ac04b35
CodeYouness <ylijassi@gmail.com>
2024-07-03 18:27:12 +0200

    added 2 new movie with class

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php 
-require_once __DIR__ . '/classes/Movie.php' 
+require_once __DIR__ . '/classes/Movie.php';
+
+$filmfiko = new Movie("l'arrivo dello scemo", "Biografia", "tanto");
+$filmnoioso = new Movie("Piove cioccolata", "Comico", "tanto corto");
 ?>
